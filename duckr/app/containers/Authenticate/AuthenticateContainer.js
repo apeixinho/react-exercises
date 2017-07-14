6e06aba66fc1ac5dac21b768d920a51f13e6079b
Jose David Baena <josedab@gmail.com>
2017-07-14 00:24:41 -0700

    REDUX-DUCKR Connecting component to redux

--- duckr/app/containers/Authenticate/AuthenticateContainer.js
+++ duckr/app/containers/Authenticate/AuthenticateContainer.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends Component {
 
@@ -12,18 +15,49 @@ class AuthenticateContainer extends Component {
   }
 
   handleAuth() {
+    const { authUser, fetchingUser,fetchingUserSuccess, fetchingUserFailure} = this.props;
+
+    fetchingUser();
     auth().then((user)=> {
-      console.log('Authed user', user)
-    })
+        fetchingUserSuccess(user.uid, user, Date.now());
+        authUser(user.uid);
+        console.log('Authed user', user)
+      })
+      .catch((error)=>fetchingUserFailure(error))
   }
 
   render() {
+    const { isFetching, error } = this.props;
     return (
-      <Authenticate isFetching={false}
-                    error=''
+      <Authenticate isFetching={isFetching}
+                    error={error}
                     onAuth={this.handleAuth}/>
     );
   }
 }
 
-export default AuthenticateContainer
+AuthenticateContainer.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  authUser: PropTypes.func.isRequired,
+  fetchingUser: PropTypes.func.isRequired,
+  fetchingUserFailure: PropTypes.func.isRequired,
+  fetchingUserSuccess: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  const {
+    isFetching,
+    error
+    } = state;
+  return {
+    isFetching,
+    error
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(userActionCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)
